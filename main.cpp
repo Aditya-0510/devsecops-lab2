e09f30163b199d8dec311e5f732513aabf274bd6
gaganrathod18 <gaganrathod080305@gmail.com>
2026-01-15 12:32:49 +0530

    quicksort

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int partition(vector<int>& arr, int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+void quickSort(vector<int>& arr, int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
 
 
 vector<int> Merge(vector<int> a,vector<int> b){
@@ -36,7 +57,6 @@ vector<int> MergeSort(vector<int> a,int start,int end){
 }
 
 void bubblesort(vector<int>&arr){
-  >>>>>>> main
       int n=arr.size();
       for(int i = 0; i < n - 1; i++) {
           for(int j = 0; j < n - i - 1; j++) {
