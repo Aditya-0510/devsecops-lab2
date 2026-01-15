8216ed81e848e8cd1ae3842a74acfdd5808f557c
lightyear256 <157786533+lightyear256@users.noreply.github.com>
2026-01-15 11:46:09 +0530

    added selection sort

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void selectionSort(vector<int>v){
+    for (int i = 0; i < v.size()-1; i++) {
+        int minIndex = i;
+
+        for (int j = i + 1; j < v.size(); j++) {
+            if (v[j] < v[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        int temp = v[i];
+        v[i] = v[minIndex];
+        v[minIndex] = temp;
+    }
+}
+
 int main(){
   
       
